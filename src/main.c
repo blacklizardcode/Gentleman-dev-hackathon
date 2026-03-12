f7dc2e99434132c565cd81ccfb01c25abfbcd57c
blacklizardcode <kimizwart@gmail.com>
2026-03-12 17:55:01 +0100

    initial commit

---
+++ src/main.c
@@ -0,0 +1,20 @@
+#include "raylib.h"
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "gentleman-dev-hackathon");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("raylib is working", 260, 210, 30, DARKGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
