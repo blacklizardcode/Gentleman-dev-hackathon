f1b271b35229ca1bbe94b2c03f7eb312a5c6126a
Hannes <hannes-swd@users.noreply.github.com>
2026-03-14 16:21:28 +0100

    feat:  globals file

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "gentleman-dev-hackathon");
+    InitWindow(screenWidth, screenHeight, "Summer game");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
